3732c107aa82a5466e7c813398e80c65a75c6cb9
Karan Chandekar <karanchandekar431@gmail.com>
2022-04-17 19:24:23 +0530

    create messanges array

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const button = document.getElementById("button");
 const toasts = document.getElementById("toasts");
+
+const messages = [
+  "Message One",
+  "Message Two",
+  "Message Three",
+  "Message Four",
+];
